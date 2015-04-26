cce674890f9c7d9b25018228263b19c79cc02249
Matthew Witman <mnwranger@hotmail.com>
2015-04-26 02:30:29 -0400

    Horribly broken (and intertwined) yet working UI code.

--- src/main/java/me/sirlag/WarframeAlerts/UI/AlertPane.java
+++ src/main/java/me/sirlag/WarframeAlerts/UI/AlertPane.java
@@ -4,18 +4,15 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 
-import me.sirlag.WarframeAlerts.Faction;
 import me.sirlag.WarframeAlerts.UI.Util.FactionParser;
 import me.sirlag.WarframeAlerts.WFAlert;
 
 import java.util.ArrayList;
 
-public class AlertPane extends Pane{
 
-    private GridPane root;
+public class AlertPane extends GridPane{
+
     private GridPane leftPane;
     private GridPane rightPane;
 
@@ -24,21 +21,36 @@ public class AlertPane extends Pane{
     private Label factionText;
     private Label planet;
     private Label node;
+    private Label description;
 
-    private ArrayList<Text>  description;
+    private ArrayList<Label> rewards;
 
     public AlertPane (WFAlert alert){
 
-        //Creates Gridpane
-        root = new GridPane();
-
+        //Start of the logo side
         leftPane = new GridPane();
 
         factionIcon = new ImageView(new Image(FactionParser.parse(alert.getFactions().get(0))));
         factionText = new Label(alert.getFactions().get(0).getFaction());
 
-        leftPane.getChildren().addAll(factionIcon);
+        leftPane.getChildren().addAll(factionIcon,factionText);
+        //Logo is done
 
+        //Start of info side
+        rightPane = new GridPane();
+        Label title = new Label("ALERT");
+        planet = new Label(alert.getPlanet());
+        node = new Label(alert.getNode());
+        description = new Label(alert.getDescription());
 
+        rightPane.getChildren().addAll(title, planet, node, description);
+
+        for(String s : alert.getRewards())
+            rightPane.getChildren().addAll(new Label(s));
+
+        this.setColumnIndex(leftPane, 0);
+        this.setColumnIndex(rightPane, 1);
+
+        this.getChildren().addAll(leftPane, rightPane);
     }
 }
