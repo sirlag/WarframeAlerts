1fa0988a48b8022e93297be930c7a78fba846331
Pieter Knickerbocker <pieterhknick@gmail.com>
2015-04-26 02:58:43 -0400

    WebInterface class method getInputStream() functioning.

--- src/main/java/me/sirlag/WarframeAlerts/WebInterface.java
+++ src/main/java/me/sirlag/WarframeAlerts/WebInterface.java
@@ -4,9 +4,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.InputStream;
 import java.io.IOException;
 
 /**
@@ -14,30 +14,23 @@ import java.io.IOException;
  * @since 04/25/2015 21:45EST
  * Class to connect via HTTP and access website RSS feed, and modify it appropriately.
  */
-public class WebInterface {
+public final class WebInterface {
     private String url;
 
     public WebInterface(String s) {
         url = s;
     }
 
-    public void getRawData() {
+    private InputStream getInputStream() throws IOException {
         HttpClient httpClient = HttpClientBuilder.create()
                 .setUserAgent("https://github.com/sirlag/WarframeAlerts")
                 .setMaxConnPerRoute(4)
                 .build();
         HttpGet httpGet = new HttpGet(url);
-        HttpResponse response = null;
-        try {
-            response = httpClient.execute(httpGet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert response != null;
+        HttpResponse response = httpClient.execute(httpGet);
         HttpEntity entity = response.getEntity();
-        System.out.println(ContentType.getOrDefault(entity));
 
-        //Intended to return String later
+        return entity.getContent();
     }
 }
 
