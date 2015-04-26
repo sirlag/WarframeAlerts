b5e47a48e3bdd8cd570629c16dd0623a33ee77a4
Pieter Knickerbocker <pieterhknick@gmail.com>
2015-04-26 15:27:56 -0400

    WebInterface fully working; added getStringFromInputStream() & @Override toString(). Gradle added commons-io.

--- src/main/java/me/sirlag/WarframeAlerts/WebInterface.java
+++ src/main/java/me/sirlag/WarframeAlerts/WebInterface.java
@@ -1,25 +1,26 @@
 package me.sirlag.WarframeAlerts;
 
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.InputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author Pieter Knickerbocker
  * @since 04/25/2015 21:45EST
  * Class to connect via HTTP and access website RSS feed, and modify it appropriately.
  */
-public final class WebInterface {
+final class WebInterface {
     private String url;
-
-    public WebInterface(String s) {
-        url = s;
-    }
+    private HttpEntity entity;
+    //Put some variables here (InputStream, et cetera)
+    WebInterface(String s) { url = s; }
 
     private InputStream getInputStream() throws IOException {
         HttpClient httpClient = HttpClientBuilder.create()
@@ -28,9 +29,23 @@ public final class WebInterface {
                 .build();
         HttpGet httpGet = new HttpGet(url);
         HttpResponse response = httpClient.execute(httpGet);
-        HttpEntity entity = response.getEntity();
-
+        entity = response.getEntity();
         return entity.getContent();
+    }
+    private String getStringFromInputStream() throws IOException {
+        Charset charSet = Charset.forName(entity.getContentEncoding().toString());
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(getInputStream(), writer, charSet);
+        return writer.toString();
+   }
+    @Override
+    public String toString() {
+        try {
+            return getStringFromInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 
